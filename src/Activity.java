c47749c53e9a7aa02d909ae14f61c6998459b32e
hhokari <57063298+hhokari@users.noreply.github.com>
2020-05-08 01:42:19 -0700

    I made these changes.

---
+++ src/Activity.java
@@ -0,0 +1,71 @@
+public class  Activity implements Action {
+    private final Entity ENTITY;
+    private final WorldModel WORLD;
+    private final ImageStore IMAGESTORE;
+    private final int REPEATCOUNT;
+
+    public Action(
+            Entity ENTITY,
+            WorldModel WORLD,
+            ImageStore IMAGESTORE,
+            int REPEATCOUNT)
+    {
+        this.ENTITY = ENTITY;
+        this.WORLD = WORLD;
+        this.IMAGESTORE = IMAGESTORE;
+        this.REPEATCOUNT = REPEATCOUNT;
+    }
+
+    private void executeAction(
+            EventScheduler scheduler)
+    {
+        switch (ENTITY.getKind()) {
+            case MINER_FULL:
+                ENTITY.executeMinerFullActivity(WORLD,
+                        IMAGESTORE, scheduler);
+                break;
+
+            case MINER_NOT_FULL:
+                ENTITY.executeMinerNotFullActivity(WORLD,
+                        IMAGESTORE, scheduler);
+                break;
+
+            case ORE:
+                ENTITY.executeOreActivity(WORLD,
+                        IMAGESTORE, scheduler);
+                break;
+
+            case ORE_BLOB:
+                ENTITY.executeOreBlobActivity(WORLD,
+                        IMAGESTORE, scheduler);
+                break;
+
+            case QUAKE:
+                ENTITY.executeQuakeActivity(WORLD,
+                        IMAGESTORE, scheduler);
+                break;
+
+            case VEIN:
+                ENTITY.executeVeinActivity(WORLD,
+                        IMAGESTORE, scheduler);
+                break;
+
+            default:
+                throw new UnsupportedOperationException(String.format(
+                        "executeActivityAction not supported for %s",
+                        ENTITY.getKind()));
+        }
+    }
+
+    public static Action createAnimationAction(Entity entity, int repeatCount) {
+        return new Action(ActionKind.ANIMATION, entity, null, null,
+                repeatCount);
+    }
+
+    public static Action createActivityAction(
+            Entity entity, WorldModel world, ImageStore imageStore)
+    {
+        return new Action(ActionKind.ACTIVITY, entity, world, imageStore, 0);
+    }
+
+}
