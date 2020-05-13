404ece8dbeed330f3ebdd0c484b4b9788463b3e7
hhokari <57063298+hhokari@users.noreply.github.com>
2020-05-13 15:57:51 -0700

    I made these changes. Removed ImageActions.java, and moved methods back to Functions. Same edit as before (some entities show up, but no movement)

--- src/Functions.java
+++ src/Functions.java
@@ -14,6 +14,13 @@ public final class Functions
 
     private static final int PROPERTY_KEY = 0;
 
+    private static final int COLOR_MASK = 0xffffff;
+    private static final int KEYED_IMAGE_MIN = 5;
+    private static final int KEYED_RED_IDX = 2;
+    private static final int KEYED_GREEN_IDX = 3;
+    private static final int KEYED_BLUE_IDX = 4;
+
+
     private static final String BGND_KEY = "background";
     private static final int BGND_NUM_PROPERTIES = 4;
     private static final int BGND_ID = 1;
@@ -60,95 +67,95 @@ public final class Functions
                 && Math.abs(p1.X - p2.X) == 1);
     }
 
-//    public static void loadImages(
-//            Scanner in, ImageStore imageStore, PApplet screen)
-//    {
-//        int lineNumber = 0;
-//        while (in.hasNextLine()) {
-//            try {
-//                processImageLine(imageStore.getImages(), in.nextLine(), screen);
-//            }
-//            catch (NumberFormatException e) {
-//                System.out.println(
-//                        String.format("Image format error on line %d",
-//                                lineNumber));
-//            }
-//            lineNumber++;
-//        }
-//    }
+    public static void loadImages(
+            Scanner in, ImageStore imageStore, PApplet screen)
+    {
+        int lineNumber = 0;
+        while (in.hasNextLine()) {
+            try {
+                processImageLine(imageStore.getImages(), in.nextLine(), screen);
+            }
+            catch (NumberFormatException e) {
+                System.out.println(
+                        String.format("Image format error on line %d",
+                                lineNumber));
+            }
+            lineNumber++;
+        }
+    }
 
-//    private static void processImageLine(
-//            Map<String, List<PImage>> images, String line, PApplet screen)
-//    {
-//        String[] attrs = line.split("\\s");
-//        if (attrs.length >= 2) {
-//            String key = attrs[0];
-//            PImage img = screen.loadImage(attrs[1]);
-//            if (img != null && img.width != -1) {
-//                List<PImage> imgs = getImages(images, key);
-//                imgs.add(img);
-//
-//                if (attrs.length >= KEYED_IMAGE_MIN) {
-//                    int r = Integer.parseInt(attrs[KEYED_RED_IDX]);
-//                    int g = Integer.parseInt(attrs[KEYED_GREEN_IDX]);
-//                    int b = Integer.parseInt(attrs[KEYED_BLUE_IDX]);
-//                    setAlpha(img, screen.color(r, g, b), 0);
-//                }
-//            }
-//        }
-//    }
+    private static void processImageLine(
+            Map<String, List<PImage>> images, String line, PApplet screen)
+    {
+        String[] attrs = line.split("\\s");
+        if (attrs.length >= 2) {
+            String key = attrs[0];
+            PImage img = screen.loadImage(attrs[1]);
+            if (img != null && img.width != -1) {
+                List<PImage> imgs = getImages(images, key);
+                imgs.add(img);
 
-//    private static List<PImage> getImages(
-//            Map<String, List<PImage>> images, String key)
-//    {
-//        List<PImage> imgs = images.get(key);
-//        if (imgs == null) {
-//            imgs = new LinkedList<>();
-//            images.put(key, imgs);
-//        }
-//        return imgs;
-//    }
+                if (attrs.length >= KEYED_IMAGE_MIN) {
+                    int r = Integer.parseInt(attrs[KEYED_RED_IDX]);
+                    int g = Integer.parseInt(attrs[KEYED_GREEN_IDX]);
+                    int b = Integer.parseInt(attrs[KEYED_BLUE_IDX]);
+                    setAlpha(img, screen.color(r, g, b), 0);
+                }
+            }
+        }
+    }
+
+    private static List<PImage> getImages(
+            Map<String, List<PImage>> images, String key)
+    {
+        List<PImage> imgs = images.get(key);
+        if (imgs == null) {
+            imgs = new LinkedList<>();
+            images.put(key, imgs);
+        }
+        return imgs;
+    }
 
     /*
       Called with color for which alpha should be set and alpha value.
       setAlpha(img, color(255, 255, 255), 0));
     */
-//    private static void setAlpha(PImage img, int maskColor, int alpha) {
-//        int alphaValue = alpha << 24;
-//        int nonAlpha = maskColor & COLOR_MASK;
-//        img.format = PApplet.ARGB;
-//        img.loadPixels();
-//        for (int i = 0; i < img.pixels.length; i++) {
-//            if ((img.pixels[i] & COLOR_MASK) == nonAlpha) {
-//                img.pixels[i] = alphaValue | nonAlpha;
-//            }
-//        }
-//        img.updatePixels();
-//    }
+    private static void setAlpha(PImage img, int maskColor, int alpha) {
+        int alphaValue = alpha << 24;
+        int nonAlpha = maskColor & COLOR_MASK;
+        img.format = PApplet.ARGB;
+        img.loadPixels();
+        for (int i = 0; i < img.pixels.length; i++) {
+            if ((img.pixels[i] & COLOR_MASK) == nonAlpha) {
+                img.pixels[i] = alphaValue | nonAlpha;
+            }
+        }
+        img.updatePixels();
+    }
 
-//    public static void load(
-//            Scanner in, WorldModel world, ImageStore imageStore)
-//    {
-//        int lineNumber = 0;
-//        while (in.hasNextLine()) {
-//            try {
-//                if (!processLine(in.nextLine(), world, imageStore)) {
-//                    System.err.println(String.format("invalid entry on line %d",
-//                            lineNumber));
-//                }
-//            }
-//            catch (NumberFormatException e) {
-//                System.err.println(
-//                        String.format("invalid entry on line %d", lineNumber));
-//            }
-//            catch (IllegalArgumentException e) {
-//                System.err.println(
-//                        String.format("issue on line %d: %s", lineNumber,
-//                                e.getMessage()));
-//            }
-//            lineNumber++;
-//        }
-//    }
+    public static void load(
+            Scanner in, WorldModel world, ImageStore imageStore)
+    {
+        int lineNumber = 0;
+        while (in.hasNextLine()) {
+            try {
+                if (!processLine(in.nextLine(), world, imageStore)) {
+                    System.err.println(String.format("invalid entry on line %d",
+                            lineNumber));
+                }
+            }
+            catch (NumberFormatException e) {
+                System.err.println(
+                        String.format("invalid entry on line %d", lineNumber));
+            }
+            catch (IllegalArgumentException e) {
+                System.err.println(
+                        String.format("issue on line %d: %s", lineNumber,
+                                e.getMessage()));
+            }
+            lineNumber++;
+        }
+    }
 
     public static boolean processLine(
             String line, WorldModel world, ImageStore imageStore)
