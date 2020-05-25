029266734896d17ef2ba0c654fc4a11508f356de
hhokari <57063298+hhokari@users.noreply.github.com>
2020-05-25 02:47:52 -0700

    I made these changes; deleted the NextPosition.java interface. SAME STUFF AS PREVIOUS COMMITT!!

--- src/NextPosition.java
+++
@@ -1,6 +0,0 @@
-public interface NextPosition extends Entity {
-    Point nextPosition(WorldModel world, Point destPos);
-    boolean move(WorldModel world, Entity target, EventScheduler scheduler);
-}
-
-
