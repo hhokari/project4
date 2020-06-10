9af10a62d341513e2a2f25c67ba8131a1560fcbb
hhokari <57063298+hhokari@users.noreply.github.com>
2020-06-09 21:40:01 -0700

    I made these changes; commented out CURRENT code; put in OLD code

--- src/Miner_Not_Full.java
+++ src/Miner_Not_Full.java
@@ -1,3 +1,92 @@
+
+import processing.core.PImage;
+
+import java.util.List;
+import java.util.Optional;
+
+public class Miner_Not_Full extends Miner {
+    private int resourceCount;
+
+    public Miner_Not_Full(
+            String ID,
+            Point position,
+            List<PImage> IMAGES,
+            int RESOURCELIMIT,
+            int resourceCount,
+            int ACTIONPERIOD,
+            int ANIMATIONPERIOD)
+    {
+        super(ID, position, IMAGES, RESOURCELIMIT, ACTIONPERIOD, ANIMATIONPERIOD);
+        this.resourceCount = resourceCount;
+    }
+
+    protected Point _nextPosition(
+            WorldModel world, Point destPos)
+    {
+        int horiz = Integer.signum(destPos.X - position.X);
+        Point newPos = new Point(position.X + horiz, position.Y);
+
+        if (horiz == 0 || world.isOccupied(newPos)) {
+            int vert = Integer.signum(destPos.Y - position.Y);
+            newPos = new Point(position.X, position.Y + vert);
+
+            if (vert == 0 || world.isOccupied(newPos)) {
+                newPos = position;
+            }
+        }
+
+        return newPos;
+    }
+
+    protected void _moveHelper(WorldModel world, Entity target, EventScheduler scheduler) {
+        resourceCount += 1;
+        world.removeEntity(target);
+        scheduler.unscheduleAllEvents(target);
+    }
+
+    protected boolean transformNotFull(
+            WorldModel world,
+            EventScheduler scheduler,
+            ImageStore imageStore)
+    {
+        if (resourceCount >= RESOURCELIMIT) {
+            Miner_Full miner = Factory.createMinerFull(ID, RESOURCELIMIT,
+                    position, ACTIONPERIOD,
+                    ANIMATIONPERIOD,
+                    IMAGES);
+
+            world.removeEntity(miner);
+            scheduler.unscheduleAllEvents(this);
+
+            world.addEntity(miner);
+            miner.scheduleActions(scheduler, world, imageStore);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    protected void _executeActivity(
+            WorldModel world,
+            ImageStore imageStore,
+            EventScheduler scheduler)
+    {
+        Optional<Entity> notFullTarget =
+                world.findNearest(position, Ore.class);
+
+        if (!notFullTarget.isPresent() || !move(world, notFullTarget.get(),
+                scheduler)
+                || !transformNotFull(world, scheduler, imageStore))
+        {
+            scheduler.scheduleEvent(this,
+                    Factory.createActivityAction(this, world, imageStore),
+                    ACTIONPERIOD);
+        }
+    }
+
+}
+/*
 import processing.core.PImage;
 
 import java.util.List;
@@ -70,3 +159,4 @@ public class Miner_Not_Full extends ResourceCountEntity {
         return world.findNearest(position, Ore.class);
     }
 }
+*/
--- src/Ore.java
+++ src/Ore.java
@@ -9,6 +9,65 @@ public class Ore extends ActiveEntity {
     private static final Random RAND = new Random();
     public static final String ORE_KEY = "ore";
     private static final String BLOB_KEY = "blob";
+    private static final String EYEBALL_KEY = "eyeball";
+    private static final String BLOB_ID_SUFFIX = " -- blob";
+    private static final int BLOB_PERIOD_SCALE = 4;
+    private static final int BLOB_ANIMATION_MIN = 50;
+    private static final int BLOB_ANIMATION_MAX = 150;
+
+    public Ore(
+            final String ID,
+            Point position,
+            final List<PImage> IMAGES,
+            final int ACTIONPERIOD)
+    {
+        super(ID, position, IMAGES, ACTIONPERIOD);
+    }
+
+    protected void _executeActivity(
+            WorldModel world,
+            ImageStore imageStore,
+            EventScheduler scheduler)
+    {
+        Point pos = position;
+
+        world.removeEntity(this);
+        scheduler.unscheduleAllEvents(this);
+
+        Ore_Blob blob = Factory.createOreBlob(ID + BLOB_ID_SUFFIX, pos,
+                ACTIONPERIOD / BLOB_PERIOD_SCALE,
+                BLOB_ANIMATION_MIN + RAND.nextInt(
+                        BLOB_ANIMATION_MAX
+                                - BLOB_ANIMATION_MIN),
+                imageStore.getImageList(BLOB_KEY));
+
+        //addded code
+//        Eyeball eyeball = Factory.createEyeball(ID + BLOB_ID_SUFFIX, pos,
+//                ACTIONPERIOD / BLOB_PERIOD_SCALE,
+//                BLOB_ANIMATION_MIN + RAND.nextInt(
+//                        BLOB_ANIMATION_MAX
+//                                - BLOB_ANIMATION_MIN),
+//                imageStore.getImageList(EYEBALL_KEY));
+
+        world.addEntity(blob);
+//        world.addEntity(eyeball);
+//        eyeball.scheduleActions(scheduler, world, imageStore);
+        blob.scheduleActions(scheduler, world, imageStore);
+    }
+
+}
+/*
+import processing.core.PImage;
+
+import java.util.List;
+import java.util.Random;
+
+
+public class Ore extends ActiveEntity {
+
+    private static final Random RAND = new Random();
+    public static final String ORE_KEY = "ore";
+    private static final String BLOB_KEY = "blob";
     private static final String BLOB_ID_SUFFIX = " -- blob";
     private static final int BLOB_PERIOD_SCALE = 4;
     private static final int BLOB_ANIMATION_MIN = 50;
@@ -49,3 +108,4 @@ public class Ore extends ActiveEntity {
     }
 
 }
+*/
