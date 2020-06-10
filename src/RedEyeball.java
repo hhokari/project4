e21f492010cdb2bc5fba62e315c43240b64ad660
hhokari <57063298+hhokari@users.noreply.github.com>
2020-06-10 01:54:58 -0700

    I made these changes; same as last commit but removed comments

--- src/RedEyeball.java
+++ src/RedEyeball.java
@@ -62,18 +62,3 @@ public class RedEyeball extends MoveEntity {
                 nextPeriod);
     }
 }
-//import processing.core.PImage;
-//
-//import java.util.List;
-//
-//public class RedEyeball extends Entity {
-//
-//    public RedEyeball(
-//            final String ID,
-//            Point position,
-//            final List<PImage> IMAGES)
-//    {
-//        super(ID, position, IMAGES);
-//    }
-//
-//}
